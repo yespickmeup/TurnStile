e60288463c94cf96af45985539a4ffbd17676742
yespickmeup <rpascua.synsoftech@gmail.com>
2016-07-20 13:40:02 +0800

    start

---
+++ src/turnstile/test2/test2.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turnstile.test2;
+
+import jssc.*;
+
+/**
+ *
+ * @author Guinness
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        //Method getPortNames() returns an array of strings. Elements of the array is already sorted.
+        String[] portNames = SerialPortList.getPortNames();
+        for (int i = 0; i < portNames.length; i++) {
+            System.out.println(portNames[i]);
+        }
+
+    }
+
+    public static void main2(String[] args) {
+        SerialPort serial = new SerialPort("COM1");
+        try {
+            //Open port
+            serial.openPort();
+            //We expose the settings. You can also use this line - serialPort.setParams(9600, 8, 1, 0);
+            serial.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            //Writes data to port
+            String message="Test";
+            serial.writeBytes(message.getBytes());
+            //Read the data of 10 bytes. Be careful with the method readBytes(), if the number of bytes in the input buffer
+            //is less than you need, then the method will wait for the right amount. Better to use it in conjunction with the
+            //interface SerialPortEventListener.
+            byte[] buffer = serial.readBytes(10);
+            //Closing the port
+            serial.closePort();
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    static SerialPort serialPort;
+
+    public static void main3(String[] args) {
+        serialPort = new SerialPort("COM1");
+        try {
+            serialPort.openPort();
+            serialPort.setParams(9600, 8, 1, 0);
+            //Preparing a mask. In a mask, we need to specify the types of events that we want to track.
+            //Well, for example, we need to know what came some data, thus in the mask must have the
+            //following value: MASK_RXCHAR. If we, for example, still need to know about changes in states 
+            //of lines CTS and DSR, the mask has to look like this: SerialPort.MASK_RXCHAR + SerialPort.MASK_CTS + SerialPort.MASK_DSR
+            int mask = SerialPort.MASK_RXCHAR;
+            //Set the prepared mask
+            serialPort.setEventsMask(mask);
+            //Add an interface through which we will receive information about events
+            serialPort.addEventListener(new SerialPortReader());
+        } catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    static class SerialPortReader implements SerialPortEventListener {
+
+        public void serialEvent(SerialPortEvent event) {
+            //Object type SerialPortEvent carries information about which event occurred and a value.
+            //For example, if the data came a method event.getEventValue() returns us the number of bytes in the input buffer.
+            if (event.isRXCHAR()) {
+                if (event.getEventValue() == 10) {
+                    try {
+                        byte buffer[] = serialPort.readBytes(10);
+                    } catch (SerialPortException ex) {
+                        System.out.println(ex);
+                    }
+                }
+            } //If the CTS line status has changed, then the method event.getEventValue() returns 1 if the line is ON and 0 if it is OFF.
+            else if (event.isCTS()) {
+                if (event.getEventValue() == 1) {
+                    System.out.println("CTS - ON");
+                } else {
+                    System.out.println("CTS - OFF");
+                }
+            } else if (event.isDSR()) {
+                if (event.getEventValue() == 1) {
+                    System.out.println("DSR - ON");
+                } else {
+                    System.out.println("DSR - OFF");
+                }
+            }
+        }
+    }
+}
